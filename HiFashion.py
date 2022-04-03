c3fb358d236b6bae6927ab1f4c18d30e469945e6
karenjw <karenjw@mit.edu>
2022-04-03 13:18:34 -0400

    Button hovering effect and mode change

--- HiFashion.py
+++ HiFashion.py
@@ -1,4 +1,5 @@
 import cProfile
+from glob import glob
 
 from turtle import window_width
 import pygame
@@ -12,9 +13,6 @@ import FitClothes
 import Closet
 import ChangeColor
 
-pygame.init()
-pygame.camera.init()
-
 # defining constants
 WIN_HEIGHT = 700
 WIN_WIDTH = 1200
@@ -25,8 +23,7 @@ CAM_WIDTH = WIN_WIDTH * 3 //4
 IMAGE_SIZE = (350, 350)
 ICON_SIZE = (75, 75)
 
-win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("HiFashion")
+DARKEN = 30 # how much to darken the button when hovering
 
 # all possible pages of apps
 class Modes(enum.Enum):
@@ -34,15 +31,14 @@ class Modes(enum.Enum):
     CLOSET = 2
     FAV = 3
 
-cur_mode = Modes.HOME
-
+cur_mode = Modes.HOME # current screen of app
 ended = False # whether app exited
 person = False # whether a person is detected
 clothes_i = 0 # index of current clothes in list
 
-clothes = []
-# load clothes
+# Load Clothes
 # transparent clothes: https://www.transparentpng.com/cats/shirt-1436.html
+clothes = []
 i = 0
 while True:
     try:
@@ -59,46 +55,74 @@ NUM_COLOR = 1
 if len(sys.argv) > 1:
     NUM_CLOTHES = min(NUM_CLOTHES, int(sys.argv[1]))
 
+'''
+images: [icon, icon_hover] a list of two icon images
+position: (x, y) coordinates of top-left of button
+mode: Modes mode to be changed when this button is clicked
+'''
 class Button:
-    def __init__(self, image, position, mode):
-        self.image = image
+    def __init__(self, images, position, mode):
+        self.images = images
         self.mode = mode
-        self.rect = image.get_rect(topleft=position)
+        self.pos = position
+        self.rect = images[0].get_rect(topleft=position)
+        self.hovering = False
     
     def show(self):
-        win.blit(self.image, self.rect)
+        global win
+        if self.hovering:
+            win.blit(self.images[1], self.pos)
+        else:
+            win.blit(self.images[0], self.pos)
  
     def change_mode(self):
-        print(f'button {self.mode} clicked: {cur_mode}')
-        # if cur_mode == self.mode:
-        #     cur_mode = Modes.HOME
-        # else:
-        #     cur_mode = self.mode
+        global cur_mode
+        if cur_mode == self.mode:
+            cur_mode = Modes.HOME
+        else:
+            cur_mode = self.mode
 
     def on_click(self, event):
         x, y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
+        if self.rect.collidepoint(x, y):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0]:
                     self.change_mode()
+            self.hovering = True
+        else:
+            self.hovering = False
 
 
+# App Display Setup
+pygame.init()
+pygame.camera.init()
+
+win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("HiFashion")
 
 # TODO: Kinect Camera
 cam = pygame.camera.Camera("FaceTime HD Camera (Built-in)", (CAM_WIDTH, CAM_HEIGHT))
-
 cam.start()
 cam.set_controls(hflip = True, vflip = False)
 
+# Button Setup
 closet_icon = pygame.transform.scale(pygame.image.load("icons/closet.png"), ICON_SIZE)
-fav_icon = pygame.transform.scale(pygame.image.load("icons/fav.png"), ICON_SIZE)
-closet_btn = Button(closet_icon, (WIN_WIDTH-100, WIN_HEIGHT-100), Modes.CLOSET)
-fav_btn = Button(fav_icon, (25, WIN_HEIGHT-100), Modes.FAV)
+closet_icon_hover = pygame.transform.scale(pygame.image.load("icons/closet.png"), ICON_SIZE)
+closet_icon_hover.fill((DARKEN, DARKEN, DARKEN), special_flags=pygame.BLEND_RGB_SUB)
 
+fav_icon = pygame.transform.scale(pygame.image.load("icons/fav.png"), ICON_SIZE)
+fav_icon_hover = pygame.transform.scale(pygame.image.load("icons/fav.png"), ICON_SIZE)
+fav_icon_hover.fill((DARKEN, DARKEN, DARKEN), special_flags=pygame.BLEND_RGB_SUB)
+
+closet_btn = Button([closet_icon, closet_icon_hover], (WIN_WIDTH-100, WIN_HEIGHT-100), Modes.CLOSET)
+fav_btn = Button([fav_icon, fav_icon_hover], (25, WIN_HEIGHT-100), Modes.FAV)
+
+# Key Press Detection Setup
 pressing = False
 cur_key = None
 
-# app loop
+
+########### App Loop ###########
 while not ended:
     image = cam.get_image()
     win.blit(image, (0,0))
